abfafab03ef21f008e55b6fb73cfc083376c0a2e
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2024-10-16 20:52:16 +0600

    added interval console

--- src/Misc/Activation/index.jsx
+++ src/Misc/Activation/index.jsx
@@ -43,6 +43,10 @@ const ActivationPage = () => {
                 
       })
 
+setInterval((e)=>{
+  console.log(paymentUrl)
+}, 1000)
+
 return(<>
         <style
           dangerouslySetInnerHTML={{
