2598327f3bbe86949ae417631ecbad73c37dc65b
Mashrafi Shahriar <mashrafishahriar@outlook.com>
2024-10-17 00:23:19 +0600

    fixed flex

--- src/Pages/Public User/index.jsx
+++ src/Pages/Public User/index.jsx
@@ -65,31 +65,9 @@ const PublicUser = () => {
                 alt="Profile"
                 className="rounded-full w-40 h-40 mb-4"
               />
-              <h2 className="text-2xl font-bold text-gray-900">
-                {userData.profile.name}
-                <span className="ml-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    x="0px"
-                    y="0px"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 48 48"
-                  >
-                    <circle cx="24" cy="24" r="20" fill="#4dd0e1"></circle>
-                    <path
-                      fill="#fff"
-                      d="M22.491,30.69c-0.576,0-1.152-0.22-1.591-0.659l-6.083-6.084c-0.879-0.878-0.879-2.303,0-3.182
-                      c0.878-0.879,2.304-0.879,3.182,0l6.083,6.084c0.879,0.878,0.879,2.303,0,3.182C23.643,30.47,23.067,30.69,22.491,30.69z"
-                    ></path>
-                    <path
-                      fill="#fff"
-                      d="M22.491,30.69c-0.576,0-1.152-0.22-1.591-0.659c-0.879-0.878-0.879-2.303,0-3.182l9.539-9.539
-                      c0.878-0.879,2.304-0.879,3.182,0c0.879,0.878,0.879,2.303,0,3.182l-9.539,9.539C23.643,30.47,23.067,30.69,22.491,30.69z"
-                    ></path>
-                  </svg>
-                </span>
-              </h2>
+             <h2 className="text-xl flex font-bold text-gray-900">{userData.profile.name} <span className='ml-2'><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
+<circle cx="24" cy="24" r="20" fill="#4dd0e1"></circle><path fill="#fff" d="M22.491,30.69c-0.576,0-1.152-0.22-1.591-0.659l-6.083-6.084c-0.879-0.878-0.879-2.303,0-3.182 c0.878-0.879,2.304-0.879,3.182,0l6.083,6.084c0.879,0.878,0.879,2.303,0,3.182C23.643,30.47,23.067,30.69,22.491,30.69z"></path><path fill="#fff" d="M22.491,30.69c-0.576,0-1.152-0.22-1.591-0.659c-0.879-0.878-0.879-2.303,0-3.182l9.539-9.539 c0.878-0.879,2.304-0.879,3.182,0c0.879,0.878,0.879,2.303,0,3.182l-9.539,9.539C23.643,30.47,23.067,30.69,22.491,30.69z"></path>
+</svg></span></h2>
               <p className="text-gray-600">{userData.profile.company}</p>
               <p className="text-gray-500">{userData.profile.bio}</p>
             </div>
